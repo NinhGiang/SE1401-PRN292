0fb6325c86a786a51f3259d555b0964717f2453d
nguyenlephuonguyen.1902@gmail.com <nguyenlephuonguyen.1902@gmail.com>
2020-06-02 21:00:14 +0700

    Student

---
+++ LAB1/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    public class Student
+    {
+        protected String _id;
+        protected String _name;
+        protected DateTime _birthdate;
+        protected bool _gender;
+        protected String _class;
+
+        public String id
+        {
+            get { return _id; }
+        }
+
+        public String name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public DateTime birthdate
+        {
+            get { return _birthdate; }
+            set { _birthdate = value; }
+        }
+        public bool gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public String classAttend
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        public Student() { }
+
+        public Student(String id, String name, DateTime birthdate, bool gender, String classA )
+        {
+            _id = id;
+            _name = name;
+            _birthdate = birthdate;
+            _gender = gender;
+            _class = classA;
+        }
+
+    }
+}
