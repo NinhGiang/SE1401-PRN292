49b9a7f315ad9ee6d2189b01500cec6c2c914b5a
Tanny Le <lengoctan289200@gmail.com>
2020-06-02 14:45:28 +0700

    create Student object

---
+++ LAB1/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Student
+{
+	protected string _id;
+	protected string _name;
+	public string ID
+	{
+		get
+		{
+			return _id;
+		}
+	}
+	public string Name
+	{
+		get { return _name; }
+		set { _name = value; }
+	}
+	public Student(string id, string Name)
+	{
+		_id = ID;
+		_name = Name;
+	}
+}
