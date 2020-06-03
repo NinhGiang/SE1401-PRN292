42c519a6a3779d190f113933233b1ebbc93ea459
Tân Nguyễn <tanntse140017@fpt.edu.vn>
2020-06-03 07:37:14 +0700

    Revert "Test conmmit"
    This reverts commit 93491c8368bb9b8eb7b89b0a2d026e92efb30297.

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -6,7 +6,7 @@ namespace LAB1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! hihi");
+            Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
     }
