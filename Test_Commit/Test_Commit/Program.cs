d88f45a41fda61b13ce692b2b38a6c0a0e2e2d8d
akiaki-47 <57090758+akiaki-47@users.noreply.github.com>
2020-06-03 07:32:17 +0700

    alo aloooooo

---
+++ Test_Commit/Test_Commit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Commit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test commit");
+        }
+    }
+}
