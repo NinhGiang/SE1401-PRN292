d2c09aacf32f09cb5b46af5dd1f64dc5ec6e719d
Phuong Nguyen <phuongnltse140404@fpt.edu.vn>
2020-06-04 13:38:15 +0700

    commit 04/06/2020

--- LAB1/Student.cs
+++ LAB1/Student.cs
@@ -4,8 +4,48 @@ namespace LAB1
 {
     public class Student
     {
-        public Student()
+        protected String _UUID;
+        protected String _name;
+        protected String _birthday;
+        protected Boolean _gender;
+        protected String _class;
+
+        public Student(String UUID, String name, String birthday, Boolean gender, String currentClass)
         {
+            this._UUID = UUID;
+            this._name = name;
+            this._birthday = birthday;
+            this._gender = gender;
+            this._class = currentClass;
+        }
+
+        public String ID
+        {
+            get{ return _UUID; }
+        }
+
+        public String Name 
+        {
+            get{ return _name; }
+            set{ _name = value; }
+        }
+
+        public String Birthday 
+        {
+            get{ return _birthday; }
+            set{ _birthday = value; }
+        }
+
+        public Boolean Gender  
+        {
+            get{ return _gender; }
+            set{ _gender = value; }
+        }
+
+        public String Class 
+        {
+            get{ return _class; }
+            set{ _class = value; }
         }
     }
 }
