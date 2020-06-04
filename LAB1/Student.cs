c152445d00448169b9eda90b21981b1b0bd56658
nguyenlephuonguyen.1902@gmail.com <nguyenlephuonguyen.1902@gmail.com>
2020-06-04 10:30:37 +0700

    lỗi

--- LAB1/Student.cs
+++ LAB1/Student.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 
 namespace LAB1
 {
@@ -12,36 +14,39 @@ namespace LAB1
         protected bool _gender;
         protected string _class;
 
-        public string id
-        {
-            get { return _id; }
-        }
+        public string GetId()
+        { return _id; }
 
-        public string name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
+        public void SetId(string value)
+        { _id = value; }
 
-        public DateTime birthdate
-        {
-            get { return _birthdate; }
-            set { _birthdate = value; }
-        }
-        public bool gender
-        {
-            get { return _gender; }
-            set { _gender = value; }
-        }
-        public string classInfo
-        {
-            get { return _class; }
-            set { _class = value; }
-        }
+        public string GetName()
+        { return _name; }
+
+        public void SetName(string value)
+        { _name = value; }
+
+        public DateTime GetBirthdate()
+        { return _birthdate; }
+
+        public void SetBirthdate(DateTime value)
+        { _birthdate = value; }
+
+        public bool GetGender()
+        { return _gender; }
+
+        public void SetGender(bool value)
+        { _gender = value; }
+
+        public string GetClassInfo()
+        { return _class; }
+
+        public void SetClassInfo(string value)
+        { _class = value; }
 
         public Student() { }
 
-        public Student(string id, string name, DateTime birthdate, bool gender, string classInfo )
+        public Student(string id, string name, DateTime birthdate, bool gender, string classInfo)
         {
             _id = id;
             _name = name;
@@ -50,10 +55,25 @@ namespace LAB1
             _class = classInfo;
         }
 
-        public static Student[] create(uint numberOfStudent)
+        public static Student[] Create(uint numberOfStudent)
         {
             Student[] result = new Student[numberOfStudent];
-            string
+            string content = File.ReadAllText(@"..\..\..\Configure.json");
+            Configure config = JsonSerializer.Deserialize<Configure>(content);
+
+            Random rnd = new Random();
+            for (uint i = 0; i < numberOfStudent; i++)
+            {
+                StudentNameConfig _ = config.StudentNameConfig;
+                int lastNameIndex = rnd.Next(_.last_name_set.Length);
+                int firstNameIndex = rnd.Next(_.first_name_set.Length);
+                int middleNameIndex = rnd.Next(_.middle_name_set.Length);
+                string fullName = _.last_name_set[lastNameIndex] + " ";
+                fullName = _.last_name_set[middleNameIndex] + " ";
+                fullName = _.last_name_set[firstNameIndex];
+                result[i] = new Student(i.ToString(), fullName, birdate, true, classInfo);
+            }
+            return result;
         }
     }
 }
