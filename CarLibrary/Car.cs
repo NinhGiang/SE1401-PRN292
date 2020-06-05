5001b814127ff6bdaa46fa01135d344722496fab
Tieu Minh Thai <tieuminh2510@gmail.com>
2020-06-05 07:23:11 +0700

    Class Library

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CarLibrary
+{
+    public enum EngineState
+    {
+        engineAlive,
+        engineDead
+    }
+    public abstract class Car
+    {
+        protected string petName;
+        protected short currSpeed;
+        protected short maxSpeed;
+        protected EngineState egnState = EngineState.engineAlive;
+        public abstract void TurboBoost();
+        public Car() { }
+        public Car(string name, short max, short curr)
+        {
+            petName = name; maxSpeed = max; currSpeed = curr;
+        }
+        public string PetName { get; set; }
+        public string CurrSpeed { get; set; }
+        public short MaxSpeed { get; }
+        public EngineState EngineState { get; }
+
+    }
+}
