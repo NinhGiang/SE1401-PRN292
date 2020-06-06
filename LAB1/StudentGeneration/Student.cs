e043b47456a5b8ebea12af393bd88189cfe5ff84
namphan <thienbao3428@gmail.com>
2020-06-06 10:38:48 +0700

    add file studenGenerate

--- LAB1/StudentGeneration/Student.cs
+++ LAB1/StudentGeneration/Student.cs
@@ -42,7 +42,7 @@ namespace StudentGeneration
         }
         public void print()
         {
-            Console.WriteLine(_id + " " + _fullname);
+            Console.WriteLine(_id+1 + " " + _fullname);
         }
     }
 }
