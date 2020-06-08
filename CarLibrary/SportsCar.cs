1fe2a1e7c1c6752f62f301156526cf8cc9e9143f
akiaki-47 <57090758+akiaki-47@users.noreply.github.com>
2020-06-08 07:55:59 +0700

    learning code
    CarClient and CarLibrary

---
+++ CarLibrary/SportsCar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CarLibrary
+{
+    public class SportsCar : Car
+    {
+        public SportsCar() {}
+        public SportsCar(string name, short max, short curr): base(name, max, curr) { }
+        public override void TurboBoost()
+        {
+            Console.WriteLine("Ramming speed: Faster is better...");
+        }
+    }
+}
