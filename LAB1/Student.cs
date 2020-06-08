b04f5d00cc2715abdcbd970188ea36cabe2b39fd
SE140111 <Admin@DESKTOP-UMAM4EV>
2020-06-08 18:07:37 +0700

    Cứu

---
+++ LAB1/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+namespace LAB1
+{
+    class Student
+    {
+        private string id;
+        private string name;
+        private string classname;
+        private DateTime birthday;
+        private bool gender;
+
+        protected string Id { get => id; set => id = value; }
+        protected string Name { get => name; set => name = value; }
+        protected string Classname { get => classname; set => classname = value; }
+        protected DateTime Birthday { get => birthday; set => birthday = value; }
+        protected bool Gender { get => gender; set => gender = value; }
+
+        public Student(string id, string name, string classname, DateTime birthday, bool gender)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Classname = classname;
+            this.Birthday = birthday;
+            this.Gender = gender;
+        }
+
+        public static Student[] Create(uint numberOfStudent)
+        {
+            Student[] result = new Student[numberOfStudent];
+            string content = File.ReadAllText(@"..\..\..\config.json");
+            config config = JsonSerializer.Deserialize<config>(content);
+            
+
+
+
+
+            return result;
+        }
+    }
+
+}
---
+++ LAB1/config.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    public class config
+    {
+        public NameConfig NameConfig
+        { get; set; }
+
+        public BirthdateConfig BirthdateConfig
+        { get; set; }
+
+        public LevelConfig LevelConfig
+        { get; set; }
+
+        public FieldConfig FieldConfig
+        { get; set; }
+    }
+    public class NameConfig
+    {
+        public string[] Last_name_set { get; set; }
+        public string[] Middle_name_set { get; set; }
+        public string[] Female_Name_Set { get; set; }
+        public string[] Male_Name_Set { get; set; }
+    }
+
+
+    public class BirthdateConfig
+    {
+        public string[] Day_set { get; set; }
+        public string[] Month_set { get; set; }
+        public string[] Year_set { get; set; }
+    }
+
+    public class LevelConfig
+    {
+        public string[] Level_Set { get; set; }
+    }
+
+    public class FieldConfig
+    {
+        public string[] field_Name_Set { get; set; }
+    }
+
+
+
+}
