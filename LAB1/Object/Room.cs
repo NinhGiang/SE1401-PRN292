a119d4348a53320554d06ec8c69930ecf2ea61a4
HieuCanDepTrai <47275163+HieuCanDepTrai@users.noreply.github.com>
2020-06-09 21:59:50 +0700

    Add  Room.cs

---
+++ LAB1/Object/Room.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LAB1
+{
+    class Room
+    {
+        protected string _id;
+        protected string _class;
+        protected int _no;
+
+        public Room()
+        {
+
+        }
+
+        public Room(string id, string classInfo, int no)
+        {
+            _id = id;
+            _class = classInfo;
+            _no = no;
+        }
+        public string GetId()
+        { return _id; }
+
+        public void SetId(string value)
+        { _id = value; }
+
+        public string GetClassInfo()
+        { return _class; }
+
+        public void SetClassInfo(string value)
+        { _class = value; }
+
+        public int GetNo()
+        { return _no; }
+
+        public void SetNo(int value)
+        { _no = value; }
+
+        
+
+        
+    }
+}
--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -6,8 +6,7 @@ namespace LAB1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            
         }
     }
 }
