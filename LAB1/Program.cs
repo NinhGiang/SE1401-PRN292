f1e0660adfeb98933b2000d1b057f92d4923a635
LongNH <LongNH@SE140159>
2020-06-09 22:01:42 +0700

    commit :)

--- LAB1/Program.cs
+++ LAB1/Program.cs
@@ -6,7 +6,7 @@ namespace LAB1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! hahahahahahahaha za warudo");
             Console.ReadLine();
         }
     }
