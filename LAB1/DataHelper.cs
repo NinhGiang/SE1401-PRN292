2bf62d230c34e32ac82e57e3501ff66edc0745f2
nguyenlephuonguyen.1902@gmail.com <nguyenlephuonguyen.1902@gmail.com>
2020-06-09 20:28:04 +0700

    Config class

--- LAB1/DataHelper.cs
+++ LAB1/DataHelper.cs
@@ -12,10 +12,26 @@ namespace LAB1
     /// </summary>
     class DataHelper
     {
+        /// <summary>
+        /// Random object to random value later
+        /// </summary>
         private static Random rnd = new Random();
+
+        /// <summary>
+        /// String of text read from Configure.json file
+        /// </summary>
         private static string content = File.ReadAllText(@"..\..\..\Configure.json");
+
+        /// <summary>
+        /// Deserialize content to different objects
+        /// </summary>
         private static Configure config = JsonSerializer.Deserialize<Configure>(content);
 
+        /// <summary>
+        /// Get list of lines from a specific .csv file
+        /// </summary>
+        /// <param name="path">A string value</param>
+        /// <returns>List of string from input filepath</returns>
         private static List<string> GetCsvData(string path)
         {
             List<string> list = new List<string>();
@@ -31,6 +47,13 @@ namespace LAB1
             return list;
         }
 
+        /// <summary>
+        /// Search for list of lines from a specific .csv file that contains search info
+        /// </summary>
+        /// <param name="path">A string value</param>
+        /// <param name="info">A string value</param>
+        /// <param name="column">An integer value</param>
+        /// <returns>List of lines from a specific .csv file that contains search info</returns>
         private static List<string> GetDataByInfo(string path, string info, int column)
         {
             List<string> list = new List<string>();
@@ -49,6 +72,12 @@ namespace LAB1
             return list;
         }
 
+        /// <summary>
+        /// Search for column index from a specific .csv file by column name
+        /// </summary>
+        /// <param name="path">A string value</param>
+        /// <param name="info">A string value</param>
+        /// <returns>The column index or -1 if not found</returns>
         private static int GetColumn(string path, string info)
         {
             StreamReader sr = new StreamReader(path);
@@ -66,6 +95,11 @@ namespace LAB1
             return -1;
         }
 
+        /// <summary>
+        /// Get list of subject by field id
+        /// </summary>
+        /// <param name="fieldId">A string value</param>
+        /// <returns>List of subject contains field id</returns>
         public static List<string> GetListOfSubjectByField(string fieldId)
         {
             string path = @"..\..\..\Subject.csv";
@@ -73,6 +107,11 @@ namespace LAB1
             return GetDataByInfo(path, fieldId, column);
         }
 
+        /// <summary>
+        /// Get list of class by level id
+        /// </summary>
+        /// <param name="levelId">A string value</param>
+        /// <returns>List of class contains level id</returns>
         public static List<string> GetListOfClassByLevel(string levelId)
         {
             string path = @"..\..\..\Class.csv";
@@ -80,36 +119,65 @@ namespace LAB1
             return GetDataByInfo(path, levelId, column);
         }
 
+        /// <summary>
+        /// Get all classes from Class.csv file
+        /// </summary>
+        /// <returns>List of classes</returns>
         public static List<string> GetListOfClass()
         {
             return GetCsvData(@"..\..\..\Class.csv");
         }
 
+        /// <summary>
+        /// Get all fields from Field.csv file
+        /// </summary>
+        /// <returns>List of fields</returns>
         public static List<string> GetListOfField()
         {
             return GetCsvData(@"..\..\..\Field.csv");
         }
 
+        /// <summary>
+        /// Get all rooms from Room.csv file
+        /// </summary>
+        /// <returns>List of rooms</returns>
         public static List<string> GetListOfRoom()
         {
             return GetCsvData(@"..\..\..\Room.csv");
         }
 
+        /// <summary>
+        /// Get all levels from Level.csv file
+        /// </summary>
+        /// <returns>List of levels</returns>
         public static List<string> GetListOfLevel()
         {
             return GetCsvData(@"..\..\..\Level.csv");
         }
 
+        /// <summary>
+        /// Get all students from Student.csv file
+        /// </summary>
+        /// <returns>List of students</returns>
         public static List<string> GetListOfStudent()
         {
             return GetCsvData(@"..\..\..\Student.csv");
         }
 
+        /// <summary>
+        /// Get all teachers from Teacher.csv file
+        /// </summary>
+        /// <returns>List of teachers</returns>
         public static List<string> GetListOfTeacher()
         {
             return GetCsvData(@"..\..\..\Teacher.csv");
         }
 
+        /// <summary>
+        /// Get a random date in a specific year
+        /// </summary>
+        /// <param name="year">An integer value</param>
+        /// <returns>A random date in a specific year</returns>
         private static DateTime GetRandomDateInYear(int year)
         {
             DateTime start = new DateTime(year, 1, 1);
@@ -118,6 +186,11 @@ namespace LAB1
             return start.AddDays(rnd.Next(range));
         }
 
+        /// <summary>
+        /// Get a random birthdate by student's level
+        /// </summary>
+        /// <param name="level">A string value</param>
+        /// <returns>A random DateTime value</returns>
         public static DateTime GetRandomBirthdayByLevel(string level)
         {
             Int32.TryParse(level, out int levelNumber);
@@ -131,6 +204,11 @@ namespace LAB1
             return GetRandomDateInYear(year);
         }
 
+        /// <summary>
+        /// Get a random name by gender
+        /// </summary>
+        /// <param name="gender">A boolean value</param>
+        /// <returns>A random string of name from config file</returns>
         public static string GetRandomNameByGender(bool gender)
         {
             string fullName;
