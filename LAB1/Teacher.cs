c15a10753080e520e63b136758853eff5a968558
Thanh Nhan <NhanNTSE140319@fpt.edu.vn>
2020-06-10 21:45:27 +0700

    Add Teacher

---
+++ LAB1/Teacher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Teacher
+    {
+        protected string _uuid;
+        protected string _name;
+        protected bool _gender;
+        protected string _field;
+
+        public Teacher(string uuid, string name, bool gender, string field)
+        {
+            _uuid = uuid ?? throw new ArgumentNullException(nameof(uuid));
+            _name = name ?? throw new ArgumentNullException(nameof(name));
+            _gender = gender;
+            _field = field ?? throw new ArgumentNullException(nameof(field));
+        }
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public bool Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+        public string Field
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+    }
+}
