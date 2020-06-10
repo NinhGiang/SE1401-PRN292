601511d6cca2cf10b5876356e3d90e9eca9517cd
Thanh Nhan <NhanNTSE140319@fpt.edu.vn>
2020-06-10 22:29:01 +0700

    Add Grade

---
+++ LAB1/Grade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Grade
+    {
+        protected string _subject;
+        protected string _student;
+        protected int _grade;
+
+        public Grade(string subject, string student, int grade)
+        {
+            _subject = subject ?? throw new ArgumentNullException(nameof(subject));
+            _student = student ?? throw new ArgumentNullException(nameof(student));
+            _grade = grade;
+        }
+        public string Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+        public string Student
+        {
+            get { return _student; }
+            set { _student = value; }
+        }
+        public int Grades
+        {
+            get { return _grade; }
+            set { _grade = value; }
+        }
+    }
+}
--- LAB1/Subject.cs
+++ LAB1/Subject.cs
@@ -6,13 +6,28 @@ namespace LAB1
 {
     class Subject
     {
+        protected string _uuid;
+        protected string _name;
         protected string _level;
         protected string _field;
 
-        public Subject(string level, string field)
+        public Subject(string uuid, string name, string level, string field)
         {
+            _uuid = uuid ?? throw new ArgumentNullException(nameof(uuid));
+            _name = name ?? throw new ArgumentNullException(nameof(name));
             _level = level ?? throw new ArgumentNullException(nameof(level));
             _field = field ?? throw new ArgumentNullException(nameof(field));
+        }
+
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
         }
         public string Level
         {
