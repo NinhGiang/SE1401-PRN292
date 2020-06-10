ef465d1ccb2a9f491087e30701c295ed40329918
Tuyet Nguyen <tuyetnta0599@gmail.com>
2020-06-10 08:26:13 +0700

    add access database

---
+++ AccessDatabase/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AccessDatabase
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.cbbStudent = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Access Database Demo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(182, 119);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.RowHeadersWidth = 51;
+            this.dataGridViewStudent.RowTemplate.Height = 24;
+            this.dataGridViewStudent.Size = new System.Drawing.Size(475, 243);
+            this.dataGridViewStudent.TabIndex = 1;
+            this.dataGridViewStudent.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cbbStudent
+            // 
+            this.cbbStudent.FormattingEnabled = true;
+            this.cbbStudent.Location = new System.Drawing.Point(255, 89);
+            this.cbbStudent.Name = "cbbStudent";
+            this.cbbStudent.Size = new System.Drawing.Size(121, 24);
+            this.cbbStudent.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Student";
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(788, 397);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbbStudent);
+            this.Controls.Add(this.dataGridViewStudent);
+            this.Controls.Add(this.label2);
+            this.Name = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.ComboBox cbbStudent;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AccessDatabase/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AccessDatabase
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DataTable student = new DataTable();
+            String sql = "SELECT * FROM Student";
+            String connection = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=D:\\CSharp\\Lab1_SE130301\\SE1401-PRN292\\AccessDatabase\\Database.accdb";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, connection);
+            adapter.Fill(student);
+            dataGridViewStudent.DataSource = student;
+            dataGridViewStudent.Columns["ClassID"].Visible = false;
+            dataGridViewStudent.Columns["Fullname"].HeaderText = "Ho va Ten";
+
+            
+            cbbStudent.DataSource = student;
+            cbbStudent.DisplayMember = "Fullname";
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
