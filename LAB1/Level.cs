7f3b5a64ae8dbf44bd76410f83027646b4b8d869
Thanh Nhan <NhanNTSE140319@fpt.edu.vn>
2020-06-10 21:33:19 +0700

    Add Field

---
+++ LAB1/Level.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Level
+    {
+        protected string _uuid;
+        protected string _field;
+
+        public Level(string uuid, string field)
+        {
+            _uuid = uuid ?? throw new ArgumentNullException(nameof(uuid));
+            _field = field ?? throw new ArgumentNullException(nameof(field));
+        }
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+        public string Field
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+    }
+}
