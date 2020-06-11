4a9f3d92e1df0a37c1e64b19023ef64d87cdb94b
LongNH <LongNH@SE140159>
2020-06-11 20:49:14 +0700

    7

---
+++ LAB1/Level.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Level
+    {
+        protected string _uuid;
+        protected string _name;
+
+        public Level(string uuid, string name)
+        {
+            _uuid = uuid;
+            _name = name;
+        }
+
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
