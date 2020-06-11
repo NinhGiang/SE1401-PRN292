309b444c5f6d52802807adc8f6fe2d6200a3532d
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 21:11:54 +0700

    New Room class

---
+++ LAB1/Room.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Room
+    {
+        private String UUID , Class ;
+        private int No;
+
+        public Room(String UUID , String Class , int No)
+        {
+            this.UUID = UUID;
+            this.Class = Class;
+            this.No = No;
+        }
+
+        public String GetUUID()
+        {
+            return this.UUID;
+        }
+
+        public void SetUUID(String UUID)
+        {
+            this.UUID = UUID;
+        }
+        public String GetClass()
+        {
+            return this.Class;
+        }
+        public void SetClsss(String Class)
+        {
+            this.Class = Class;
+        }
+
+        public int GetNo()
+        {
+            return this.No;
+        }
+
+        public void SetNo(int No)
+        {
+            this.No = No;
+        }
+    }
+}
