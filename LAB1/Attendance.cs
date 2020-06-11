f497e72859c0139350bc1494e206a565e04f3af5
LongNH <LongNH@SE140159>
2020-06-11 21:11:22 +0700

    10

---
+++ LAB1/Attendance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Attendance
+    {
+        protected string _teacher;
+        protected string _class;
+        protected string _subject;
+
+        public Attendance(string teacher, string @class, string subject)
+        {
+            _teacher = teacher;
+            _class = @class;
+            _subject = subject;
+        }
+
+        public string Teacher
+        {
+            get { return _teacher; }
+            set { _teacher = value; }
+        }
+
+        public string Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        public string Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+    }
+}
