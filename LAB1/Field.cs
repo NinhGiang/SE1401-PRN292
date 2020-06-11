932869492d5eb4e1dec29ab687fce73d885caa53
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 21:46:40 +0700

    Field Class

---
+++ LAB1/Field.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Field
+    {
+        protected String UUID , Name;
+
+        public Field(String UUID , String Name)
+        {
+            this.UUID = UUID;
+            this.Name = Name;
+        }
+
+        public String GetUUID()
+        {
+            return this.UUID;
+        }
+        public void SetUUID(String UUID)
+        {
+            this.UUID = UUID;
+        }
+
+        public String GetName()
+        {
+            return this.Name;
+        }
+
+        public void SetName(String Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
