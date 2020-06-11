00e4f74c4aae6dbe6d1c509a6112acedda678e7c
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 22:00:51 +0700

    Attendance Class

---
+++ LAB1/Attendance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Attendance
+    {
+        public String Teacher, Class, Subject;
+
+        public Attendance(String Teacher , String Class , String Subject)
+        {
+            this.Teacher = Teacher;
+            this.Class = Class;
+            this.Subject = Subject;
+        }
+
+        public String GetTeacher()
+        {
+            return this.Teacher;
+        }
+
+        public void SetTeacher(String Teacher)
+        {
+            this.Teacher = Teacher;
+        }
+
+        public String GetClass()
+        {
+            return this.Class;
+        }
+
+        public void SetClass(String Class)
+        {
+            this.Class = Class;
+        }
+
+        public String GetSubject()
+        {
+            return this.Subject;
+        }
+
+        public void SetSubject(String Subject)
+        {
+            this.Subject = Subject;
+        }
+
+    }
+}
