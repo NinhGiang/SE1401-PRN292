6147decc839d335f8da9fbcafe23480079ecb981
LongNH <LongNH@SE140159>
2020-06-11 13:49:06 +0700

    4

---
+++ LAB1/Teacher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Teacher
+    {
+        protected string _uuid;
+        protected string _name;
+        protected bool _gender;
+        protected string _field;
+
+        public Teacher(string uuid, string name, bool gender, string field)
+        {
+            _uuid = uuid;
+            _name = name;
+            _gender = gender;
+            _field = field;
+        }
+
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public bool Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public string Field
+        {
+            get { return _field; }
+            set { _field = value; }
+        }
+    }
+}
