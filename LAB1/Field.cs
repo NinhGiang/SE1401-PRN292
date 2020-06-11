a2371ee93e2c3d7c465f09a712c6a755826d32f3
LongNH <LongNH@SE140159>
2020-06-11 23:15:18 +0700

    Generate fields

--- LAB1/Field.cs
+++ LAB1/Field.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
 
 namespace LAB1
 {
@@ -25,6 +27,28 @@ namespace LAB1
         {
             get { return _name; }
             set { _name = value; }
+        }
+
+        private static String jsonFile = File.ReadAllText(@"..\..\..\Configure.json");
+        private static Configure config = JsonSerializer.Deserialize<Configure>(jsonFile);
+
+        public static string[] getField()
+        {
+            return config.FieldConfig.field_set;
+        }
+
+        public static Field[] GenerateField()
+        {
+            String[] data = getField();
+            Field[] fieldList = new Field[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                String uuid = Guid.NewGuid().ToString();
+                String name = data[i];
+                Field field = new Field(uuid, name);
+                fieldList[i] = field;
+            }
+            return fieldList;
         }
     }
 }
