74138ab1f5133c9626498e02c926e556d50cbb26
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 21:29:05 +0700

    New Class

---
+++ LAB1/Class.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace LAB1
+{
+    class Class
+    {
+        protected String Level , Room , Name ;
+
+        public Class(String Level , String Room , String Name)
+        {
+            this.Level = Level;
+            this.Room = Room;
+            this.Name = Name;
+        }
+
+        public String GetLevel()
+        {
+            return this.Level;
+        }
+
+        public void SetLevel(String Level)
+        {
+            this.Level = Level;
+        }
+
+        public String GetRoom()
+        {
+            return this.Room;
+        }
+
+        public void SetRoom(String Room)
+        {
+            this.Room = Room;
+        }
+
+        public String GetName()
+        {
+            return this.Name;
+        }
+        public void SetName(String Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
