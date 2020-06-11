3750b49ced4f84a3c59af6d22a7d862c970aec24
Shiro <toshirou.ha@gmail.com>
2020-06-12 00:51:35 +0700

    add room, class

---
+++ SchoolGenerating/Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolGenerating
+{
+    class Class
+    {
+        private String classID { get { return level; } set { this.classID = value; } }
+        private String level{ get { return level; } set { this.level = value; } }
+        private String room { get { return level; } set { this.room = value; } }
+        private String className { get { return level; } set { this.className = value; } }
+
+        public Class(string classID, string level, string room, string className)
+        {
+            this.classID = classID;
+            this.level = level;
+            this.room = room;
+            this.className = className;
+        }
+
+        //generate Class
+        public static Class[] Create(uint number_of_Classes)
+        {
+            List<Class> class_list = new List<Class>();
+            for (int i = 0; i < class_list.lengh; i++)
+            {
+
+            }
+            return class_list.ToArray();
+        }
+    }
+}
---
+++ SchoolGenerating/Room.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolGenerating
+{
+    class Room
+    {
+        private string roomID { get { return roomID; } set { roomID = value; } };
+        private string className { get { return className; } set { className = value; } };
+        private int no { get { return no; } set { no = value; } };
+
+        public Room()
+        {
+        }
+
+        public Room(string roomID, string className, int no)
+        {
+            this.roomID = roomID;
+            this.className = className;
+            this.no = no;
+        }
+
+
+    }
+}
