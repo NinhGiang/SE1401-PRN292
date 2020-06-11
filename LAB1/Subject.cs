7d2e510ec00acb75987503bb785117cdac34e637
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 21:40:41 +0700

    Subject Class

---
+++ LAB1/Subject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Subject
+    {
+        protected String Level, Field;
+
+        public Subject(String Level , String Field)
+        {
+            this.Level = Level;
+            this.Field = Field;
+        }
+
+        public String GetLevel()
+        {
+            return this.Level;
+        }
+
+        public void SetString(String Level)
+        {
+            this.Level = Level;
+        }
+
+        public String GetField()
+        {
+            return this.Field;
+        }
+
+        public void SetField(String Field)
+        {
+            this.Field = Field;
+        }
+    }
+}
