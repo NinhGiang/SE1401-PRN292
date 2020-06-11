54bff3768cf016731151ee6eb3bc7893e95659fa
Tieu Minh Thai <tieuminh2510@gmail.com>
2020-06-11 22:28:06 +0700

    Access database

---
+++ AccessDatabase/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AccessDatabase
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DataTable student = new DataTable();
+            String sql = "Select * from Student";
+            String connector = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=D:\\Documents\\Database.accdb";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, connector);
+            adapter.Fill(student);
+            dataGridViewStudent.DataSource = student;
+            dataGridViewStudent.Columns["ClassID"].Visible = false;
+            dataGridViewStudent.Columns["Fullname"].HeaderText = "Họ và tên";
+            comboBoxStudent.DataSource = student;
+            comboBoxStudent.DisplayMember = "Fullname";
+        }
+    }
+}
