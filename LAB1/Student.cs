d889621ee4edd20c3d03ee6bd15a9b48a1b4b571
LongNH <LongNH@SE140159>
2020-06-11 13:31:19 +0700

    Commit 2 :)

---
+++ LAB1/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Student
+    {
+        protected string _uuid;
+        protected string _name;
+        protected DateTime _birthday;
+        protected bool _gender;
+        protected string _class;
+
+        public Student(string uuid, string name, DateTime birthday, bool gender, string Class)
+        {
+            _uuid = uuid;
+            _name = name;
+            _birthday = birthday;
+            _gender = gender;
+            _class = Class;
+        }
+
+
+    }
+}
