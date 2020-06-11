8a079674067546708582e3e53ba5d88979083e32
LongNH <LongNH@SE140159>
2020-06-11 20:44:11 +0700

    6

---
+++ LAB1/Class.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Class
+    {
+        protected string _uuid;
+        protected string _level;
+        protected string _room;
+        protected string _name;
+
+        public Class(string uuid, string level, string room, string name)
+        {
+            _uuid = uuid;
+            _level = level;
+            _room = room;
+            _name = name;
+        }
+
+        public string UUID
+        {
+            get { return _uuid; }
+            set { _uuid = value; }
+        }
+
+        public string Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+
+        public string Room
+        {
+            get { return _room; }
+            set { _room = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
