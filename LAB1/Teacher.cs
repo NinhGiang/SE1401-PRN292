685a8376d6a9548e71fb7b9d87cd5a109eff3bcd
Hưng Giang <gianggiahung06102000@gmail.com>
2020-06-11 21:55:31 +0700

    Teacher Class

---
+++ LAB1/Teacher.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Teacher
+    {
+        protected String UUID , Name , Field;
+        protected Boolean Gender;
+
+        public Teacher(String UUID , String Name , Boolean Gender , String Field)
+        {
+            this.UUID = UUID;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Field = Field;
+        }
+
+        public String GetUUID()
+        {
+            return this.UUID;
+        }
+
+        public void SetUUID(String UUID)
+        {
+            this.UUID = UUID;
+        }
+
+        public String GetName()
+        {
+            return this.Name;
+        }
+
+        public void SetName(String Name)
+        {
+            this.Name = Name;
+        }
+
+        public Boolean GetGender()
+        {
+            return this.Gender;
+        }
+
+        public void SetGender(Boolean Gender)
+        {
+            this.Gender = Gender;
+        }
+
+        public String GetField()
+        {
+            return this.Field;
+        }
+
+        public void SetField(String Field)
+        {
+            this.Field = Field;
+        }
+    }
+}
