7e396123dabaf376b1efbef1c9546020d95ca1e4
LongNH <LongNH@SE140159>
2020-06-11 22:01:22 +0700

    13

---
+++ LAB1/Configure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Configure
+    {
+        
+    }
+
+    class NameConfig
+    {
+        public string[] male_first_name { get; set; }
+        public string[] female_first_name { get; set; }
+        public string[] middle_name { get; set; }
+        public string[] last_name { get; set; }
+    }
+
+    class LevelConfig
+    {
+        public string[] level_set { get; set; }
+    }
+
+    class FieldConfig
+    {
+        public string[] field_set { get; set; }
+    }
+}
