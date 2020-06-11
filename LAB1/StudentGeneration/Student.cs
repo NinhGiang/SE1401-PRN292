206cb003bdf32e885a25f5f2b492996e4d6be0f7
namphan <thienbao3428@gmail.com>
2020-06-11 21:18:09 +0700

    update data

--- LAB1/StudentGeneration/Student.cs
+++ LAB1/StudentGeneration/Student.cs
@@ -9,15 +9,24 @@ namespace StudentGeneration
 {
     class Student
     {
-        protected string _id;
+        protected string _UUID;
         protected string _fullname;
-        public string ID { get { return _id; } }
+        protected string _dayofbirth;
+        protected string _gender;
+        protected string _class;
+        public string ID { get { return _UUID; } }
         public string FullName { get { return _fullname; } }
+        public string DayOfBirth { get { return _dayofbirth; } }
+        public string Gender { get{ return _gender; } }
+        public string Class { get{ return _class; } }
 
-        protected Student(string ID, string FullName)
+        protected Student(string ID, string FullName,string Dayofbirth,string Gender,string Class)
         {
-            _id = ID;
+            _UUID = ID;
             _fullname = FullName;
+            _dayofbirth = Dayofbirth;
+            _gender = Gender;
+            _class = Class;
         }
 
         public static Student[] Create(uint number_student)
@@ -29,20 +38,31 @@ namespace StudentGeneration
             Random rnd = new Random();
             for (uint i = 0; i < number_student; i++)
             {
-                NameConfig _ = config.NameConfig;
-                int last_name_index = rnd.Next(_.last_name_set.Length);
-                int first_name_index = rnd.Next(_.first_name_set.Length);
-                int middle_name_index = rnd.Next(_.middle_name_set.Length);
-                string full_name = _.last_name_set[last_name_index] + " ";
-                full_name += _.middle_name_set[middle_name_index] + " ";
-                full_name += _.first_name_set[first_name_index];
-                result[i] = new Student(i.ToString(), full_name);
+                NameConfig name = config.NameConfig;
+                BirthDayConfig dob = config.BirthdayConfig;
+                int gender_index = rnd.Next(config.GenderConfig.Gender_set.Length);
+                ClassConfig classname = config.ClassConfig;
+                //if Male
+                if (gender_index == 0)
+                {
+                    //name
+                    int last_name_index = rnd.Next(name.last_Male_set.Length);
+                    int first_name_index = rnd.Next(name.first_Male_set.Length);
+                    int middle_name_index = rnd.Next(name.middle_Male_set.Length);
+                    string full_name = name.last_Male_set[last_name_index] + " ";
+                    full_name += name.middle_Male_set[middle_name_index] + " ";
+                    full_name += name.first_Male_set[first_name_index];
+                    //birthday
+                  
+                    
+                }
+
             }
             return result;
         }
         public void print()
         {
-            Console.WriteLine(_id+1 + " " + _fullname);
+            Console.WriteLine(_UUID +1 + " " + _fullname);
         }
     }
 }
