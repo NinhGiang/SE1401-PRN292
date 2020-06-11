d46980a6d510d1d4cab2448b46b09ece7e70f306
Tieu Minh Thai <tieuminh2510@gmail.com>
2020-06-11 22:20:44 +0700

    json serialization

--- StudentGeneration/School.cs
+++ StudentGeneration/School.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace StudentGeneration
 {
     class School
     {
         private List<Student> _students_list;
+        public List<Student> Students {get { return _students_list; } }
         public School(Student[] students)
         {
             _students_list = new List<Student>(students);
         }
         public void save(string filename)
         {
-            String content = "ID, Fullname\n";
-            foreach(Student student in _students_list)
+            if(Path.GetExtension(filename) == ".csv")
             {
-                content += student.ID + ", " + student.FullName + "\n";
+                String content = "ID, Fullname\n";
+                foreach (Student student in _students_list)
+                {
+                    content += student.ID + ", " + student.FullName + "\n";
+                }
+                File.WriteAllText(filename, content);
+            } else if (Path.GetExtension(filename) == ".json")
+            {
+
+                String content = JsonConvert.SerializeObject(this, Formatting.Indented);
+                File.WriteAllText(filename, content);
             }
-            File.WriteAllText(filename, content);
+
         }
     }
 }
