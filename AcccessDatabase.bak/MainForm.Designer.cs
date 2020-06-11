7bece02f84272fb07c4fcfe6eeea91d7fa5a1a41
Tieu Minh Thai <tieuminh2510@gmail.com>
2020-06-11 21:45:49 +0700

    All commit

---
+++ AcccessDatabase.bak/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AcccessDatabase
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAccess = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(216, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database Accessing";
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(12, 95);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.Size = new System.Drawing.Size(698, 351);
+            this.dataGridViewStudent.TabIndex = 1;
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.FormattingEnabled = true;
+            this.comboBoxClass.Location = new System.Drawing.Point(50, 68);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(192, 21);
+            this.comboBoxClass.TabIndex = 2;
+            this.comboBoxClass.SelectedIndexChanged += new System.EventHandler(this.comboBoxClass_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Class";
+            // 
+            // buttonAccess
+            // 
+            this.buttonAccess.Location = new System.Drawing.Point(259, 67);
+            this.buttonAccess.Name = "buttonAccess";
+            this.buttonAccess.Size = new System.Drawing.Size(95, 21);
+            this.buttonAccess.TabIndex = 4;
+            this.buttonAccess.Text = "Access";
+            this.buttonAccess.UseVisualStyleBackColor = true;
+            this.buttonAccess.Click += new System.EventHandler(this.buttonAccess_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 458);
+            this.Controls.Add(this.buttonAccess);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxClass);
+            this.Controls.Add(this.dataGridViewStudent);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+        private System.Windows.Forms.ComboBox comboBoxClass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAccess;
+    }
+}
+
---
+++ AcccessDatabase.bak/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AcccessDatabase
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private DataTable getTable(String sql)
+        {
+            DataTable table = new DataTable();
+            String connector = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=D:\\Downloads\\Database.accdb";
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, connector);
+            adapter.Fill(table);
+            return table;
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DataTable class_table = getTable("Select * from Lop");
+            comboBoxClass.DataSource = class_table;
+            comboBoxClass.DisplayMember = "Name";
+        }
+
+        private void buttonAccess_Click(object sender, EventArgs e)
+        {
+            DataTable class_table = (DataTable)comboBoxClass.DataSource;
+            int selected_index = comboBoxClass.SelectedIndex;
+            DataRow selected_row = class_table.Rows[selected_index];
+            DataTable student_table = getTable("Select * from HOC_SINH Where Ma_lop = " + selected_row["ID"]);
+            dataGridViewStudent.DataSource = student_table;
+            dataGridViewStudent.Columns["Ma_lop"].Visible = false; 
+        }
+
+        private void comboBoxClass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonAccess_Click(sender, e);
+        }
+    }
+}
