d57102a1674cf0eb00f8b054bf3c345fe137d1d8
LongNH <LongNH@SE140159>
2020-06-11 21:18:30 +0700

    11

---
+++ LAB1/Grade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class Grade
+    {
+        protected string _subject;
+        protected string _student;
+        protected int _grade;
+
+        public Grade(string subject, string student, int grade)
+        {
+            _subject = subject;
+            _student = student;
+            _grade = grade;
+        }
+
+        public string Subject
+        {
+            get { return _subject; }
+            set { _subject = value; }
+        }
+
+        public string Student
+        {
+            get { return _student; }
+            set { _student = value; }
+        }
+
+        public int Grading
+        {
+            get { return _grade; }
+            set { _grade = value; }
+        }
+    }
+}
