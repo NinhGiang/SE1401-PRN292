b54060cf37aebba1b839eefc1c790b2f3cdb92e2
akiaki-47 <57090758+akiaki-47@users.noreply.github.com>
2020-06-12 13:07:43 +0700

    update

--- StudentGeneration/School.cs
+++ StudentGeneration/School.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace StudentGeneration
 {
@@ -15,10 +16,20 @@ namespace StudentGeneration
         public void save(string filename)
         {
             String content = "ID, Fullname\n";
-            foreach(Student student in _students_list)
+            if(Path.GetExtension(filename) == ".csv")
             {
-                content += student.ID + ", " + student.FullName + "\n";
+                foreach (Student student in _students_list)
+                {
+                    content += student.ID + ", " + student.FullName + "\n";
+                }
+            } else if (Path.GetExtension(filename) == ".json")
+            {
+                foreach (Student student in _students_list)
+                {
+                    content += student.ID + ", " + student.FullName + "\n";
+                }
             }
+            
             File.WriteAllText(filename, content);
         }
     }
