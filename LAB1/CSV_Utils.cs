4414adba5d1cda237fabbfb72a2cfcb37ae850cf
LongNH <LongNH@SE140159>
2020-06-13 19:21:19 +0700

    Added CSV_Utils

---
+++ LAB1/CSV_Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB1
+{
+    class CSV_Utils
+    {
+        private static string attendanceCSV;
+        private static string classCSV;
+        private static string FieldCSV;
+        private static string GradeCSV;
+        private static string LevelCSV;
+        private static string RoomCSV;
+        private static string studentCSV;
+        private static string subjectCSV;
+        private static string teacherCSV;
+
+
+    }
+}
