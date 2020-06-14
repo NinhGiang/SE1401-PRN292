d19709bb3aafa0fc8e236cb64859456af37acbbd
Tân Nguyễn <tanntse140017@fpt.edu.vn>
2020-06-14 10:24:02 +0700

    Create Attendance

--- LAB1/Attendance.cs
+++ LAB1/Attendance.cs
@@ -6,8 +6,15 @@ namespace LAB1
 {
     class Attendance
     {
-        protected string teacher;
-        protected string _class;
-        protected string subject;
+        protected string _teacher;
+        protected string _className;
+        protected string _subject;
+
+        public Attendance(string teacher, string className, string subject)
+        {
+            _teacher = teacher;
+            _className = className;
+            _subject = subject;
+        }
     }
 }
